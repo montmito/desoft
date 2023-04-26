abbb5f92f2cf7848cd4d245c96cd525177e6bd43
montmito <caiommb@al.insper.edu.br>
2023-04-26 11:19:23 -0300

    posiciona frota

--- batalha_naval.py
+++ batalha_naval.py
@@ -25,3 +25,17 @@ def faz_jogada(tabuleiro, linha, coluna):
     elif tabuleiro[linha][coluna] == 0:
         tabuleiro[linha][coluna] = '-'
     return tabuleiro
+
+
+def posiciona_frota(frota):
+    
+    grid = [[0 for i in range(10)] for j in range(10)]
+    
+   
+    for nome_navio, posicoes_navio in frota.items():
+        for posicoes in posicoes_navio:
+            for posicao in posicoes:
+                linha, coluna = posicao
+                grid[linha][coluna] = 1
+    
+    return grid
