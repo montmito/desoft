24e14cd1ae7b931e9997132b00be7210fae8b141
montmito <caiommb@al.insper.edu.br>
2023-04-27 21:15:04 -0300

    quantas embarcacoes afundadas

--- batalha_naval.py
+++ batalha_naval.py
@@ -39,3 +39,18 @@ def posiciona_frota(frota):
                 grid[linha][coluna] = 1
     
     return grid
+
+
+def afundados(frota, tabuleiro):
+    navios_afundados = 0
+    for nome_navio, posicoes_navio in frota.items():
+        for posicao_navio in posicoes_navio:
+            afundou = True
+            for coordenadas in posicao_navio:
+                if tabuleiro[coordenadas[0]][coordenadas[1]] != 'X':
+                    afundou = False
+                    break
+            if afundou:
+                navios_afundados+=1
+
+    return navios_afundados
