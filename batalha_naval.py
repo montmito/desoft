5e38d68f01e589b93d84672031da22a9edfe743a
montmito <caiommb@al.insper.edu.br>
2023-04-30 21:20:44 -0300

    valida posição

--- batalha_naval.py
+++ batalha_naval.py
@@ -54,3 +54,16 @@ def afundados(frota, tabuleiro):
                 navios_afundados+=1
 
     return navios_afundados
+
+
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    for posicao in posicoes:
+        linha, coluna = posicao
+        if linha < 0 or linha > 9 or coluna < 0 or coluna > 9:
+            return False
+        for navio in frota.values():
+            for posicoes_navio in navio:
+                if posicao in posicoes_navio:
+                    return False
+    return True
